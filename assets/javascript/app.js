175e254d1713ef5e6f394a4dafcbc29bb33b37f9
Jan Balangue <jantonio.balangue@gmail.com>
2018-03-10 18:15:33 -0800

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB_Z3QP7CH95uVe5HnFRP49wR6PzzQuGAo",
+    authDomain: "rps-multiplayer-1d8cf.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-1d8cf.firebaseio.com",
+    projectId: "rps-multiplayer-1d8cf",
+    storageBucket: "",
+    messagingSenderId: "563863947172"
+  };
+  firebase.initializeApp(config);
