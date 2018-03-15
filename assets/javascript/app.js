9d22c24819420cde595da81d6af255835b337c8a
Jan Balangue <jantonio.balangue@gmail.com>
2018-03-14 22:36:09 -0700

    Modified style.css, app.js and index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,94 +1,242 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB_Z3QP7CH95uVe5HnFRP49wR6PzzQuGAo",
+    authDomain: "rps-multiplayer-1d8cf.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-1d8cf.firebaseio.com",
+    projectId: "rps-multiplayer-1d8cf",
+    storageBucket: "",
+    messagingSenderId: "563863947172"
+};
+
+firebase.initializeApp(config);
+
 var database = firebase.database();
 
-var connectionsRef = database.ref("/connections");
-var connectedRef = database.ref(".info/connected");
-var rpsRef = database.ref().child("rps");
-var chatRef = database.ref().child("chat");
-
-connectedRef.on("value", function (snapshot) {
-    if (snapshot.val()) {
-        var con = connectionsRef.push(true);
-        con.onDisconnect().remove();
-    }
+firebase.auth().signInAnonymously().catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode + " " + errorMessage);
+    // ...
 });
 
-connectionsRef.on("value", function (snapshot) {
-    if (snapshot.numChildren() === 2) {
-        // start game only if there are 2 players
-    }
-});
-
-
+var playersRef = database.ref().child("/players/");
+var chatRef = database.ref().child("/chatroom/");
+var yourName;
+var move;
+var user = firebase.auth().currentUser;
 
 var player = [{
-    name: "Player 1",
+    name: "",
     choice: "",
     wins: 0,
-    losses: 0
+    losses: 0,
+    ties: 0
 },
 {
-    name: "Player 2",
+    name: "",
     choice: "",
     wins: 0,
-    losses: 0
+    losses: 0,
+    ties: 0
 }];
-var name = "";
 
-database.ref().on("value", function (snapshot) {
-    if (snapshot.child("rps").exists()) {
-        console.log(snapshot.val().rps.player1name);
-        console.log(snapshot.val().rps.player1choice);
-        console.log(snapshot.val().rps.player1wins);
-        console.log(snapshot.val().rps.player1losses);
-        console.log(snapshot.val().rps.player2name);
-        console.log(snapshot.val().rps.player2choice);
-        console.log(snapshot.val().rps.player2wins);
-        console.log(snapshot.val().rps.player2losses);
-
-        $("#player-1-name").text(snapshot.val().player1name);
-        $("#player-1-choice").text(snapshot.val().player1choice);
-        $("#player-1-wins").text(snapshot.val().player1wins);
-        $("#player-1-losses").text(snapshot.val().player1losses);
-        $("#player-2-name").text(snapshot.val().player2name);
-        $("#player-2-choice").text(snapshot.val().player2choice);
-        $("#player-2-wins").text(snapshot.val().player2wins);
-        $("#player-2-losses").text(snapshot.val().player2losses);
+database.ref('/players/').on("value", function (snapshot) {
+    if (snapshot.child('player1').exists()) {
+        player[0] = snapshot.val().player1;
+        $("#player-1-name").text(player[0].name);
+        $("#player-1-wins").text(player[0].wins);
+        $("#player-1-losses").text(player[0].losses);
+        $("#player-1-ties").text(player[0].ties);
     } else {
-        var rpsRef = database.ref().child("rps");
-        rpsRef.player1name = "";
-        rpsRef.player1choice = "";
-        rpsRef.player1wins = 0;
-        rpsRef.player1losses = 0;
-        rpsRef.player2name = "";
-        rpsRef.player2choice = "";
-        rpsRef.player2wins = 0;
-        rpsRef.player2losses = 0;
+        // no player 1
+        $("#player-1-name").text("Waiting for player 1");
     }
-}, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
+    if (snapshot.child('player2').exists()) {
+        player[1] = snapshot.val().player2;
+        $("#player-2-name").text(player[1].name);
+        $("#player-2-wins").text(player[1].wins);
+        $("#player-2-losses").text(player[1].losses);
+        $("#player-2-ties").text(player[1].ties);
+    } else {
+        // no player 2
+        $("#player-2-name").text("Waiting for player 2");
+    }
+
+    if (!snapshot.child('player1').exists() && !snapshot.child('player2').exists()) {
+        $("#chat-area").empty();
+        $("#move-section").empty();
+        database.ref('/move/').remove()
+        database.ref('/chatroom/').remove();
+    }
 });
+
+
 
 $("#start-button").on("click", function (event) {
     event.preventDefault(); // don't refresh the page
-    name = $('input[type=text]#player-name').val().trim();
-    if (name === "") {
-        name = "Player " + (playerIndex + 1);
+    yourName = $('input[type=text]#player-name').val().trim();
+    yourName = yourName.substring(0, 25);
+    console.log(yourName);
+    if (yourName !== "" && player[0].name === "" && player[1].name === "") {
+        // add player 1
+        player[0] = {
+            name: yourName,
+            choice: "",
+            wins: 0,
+            losses: 0,
+            ties: 0,
+        }
+        database.ref().child('/players/player1').set(player[0]);
+        database.ref().child('/players/player1').onDisconnect().remove();
+    } else if (player[0].name !== "" && player[1].name === "") {
+        // add player 2
+        player[1] = {
+            name: yourName,
+            choice: "",
+            wins: 0,
+            losses: 0,
+            ties: 0
+        }
+        database.ref().child('/players/player2').set(player[1]);
+        database.ref().child('/players/player2').onDisconnect().remove();
     }
-    if (name.length > 20) {
-        name = name.substring(0, 20);
-    }
-    console.log(name);
-    if (rpsRef.player1name !== "") {
-        database.ref().set({
-            player1name: name
-        });
-        $("#player-1-name").text(name);
-        $("#hello-message-section").text("Hi " + name + "! You are player 1.")
-    } else {
-        database.ref().set({
-            player2name: name
-        });
-        $("#player-2-name").text(name);
-        $("#hello-message-section").text("Hi " + name + "! You are player 2.")
+    database.ref().child('/move/').set(1);
+    move = 1;
+});
+
+
+database.ref("/players/").on("child_added", function (snapshot) {
+    var chatMessage = snapshot.val().name + " is now connected.\n";
+    $("#chat-area").append(chatMessage);
+});
+
+database.ref("/players/").on("child_removed", function (snapshot) {
+    var chatMessage = snapshot.val().name + " is now disconnected\n";
+    $("#chat-area").append(chatMessage);
+});
+
+database.ref("/move/").on("value", function (snapshot) {
+    if (snapshot.val() === 1) {
+        // player 1's move
+        $("#move-section").html("Waiting for " + player[0].name + "'s move.");
+    } else if (snapshot.val() === 2) {
+        // player 2's move
+        $("#move-section").html("Waiting for " + player[1].name + "'s move.");
     }
 });
+
+$("#send-button").on("click", function (event) {
+    event.preventDefault();
+    var message = $("#chat-message").val().trim();
+    if (message !== "") {
+        database.ref().child("/chatroom/").set(message);
+    }
+})
+
+database.ref("/chatroom/").on("value", function (snapshot) {
+    var message = snapshot.val();
+    if (message !== null) {
+        $("#chat-area").append(message + "\n");
+    }
+});
+
+$("#player-1-choice").on("click", ".choice", function (event) {
+    event.preventDefault();
+    console.log("player 1 chooses");
+    if (move === 1) {
+        player[0].choice = event.target.id;
+
+        $(this).addClass("selected");
+        console.log(player[0].choice);
+
+        database.ref().child("/players/player1/choice/").set(player[0].choice);
+        move = 2;
+        database.ref("/move/").set(move);
+    }
+})
+
+$("#player-2-choice").on("click", ".choice", function (event) {
+    event.preventDefault();
+    console.log("player 2 chooses");
+    if (move === 2) {
+        player[1].choice = event.target.id;
+
+        $(this).addClass("selected");
+        console.log(player[1].choice);
+
+        database.ref().child("/players/player2/choice/").set(player[1].choice);
+        database.ref("/move/").remove();
+        getResult();
+        move = 1;
+    }
+})
+
+function getResult() {
+    if (player[0].choice === "player-1-rock") {
+        if (player[1].choice === "player-2-rock") {
+            $("#move-section").text("Two rocks.\nIt's a draw!");
+            $("#player-1-rock").removeClass("selected");
+            $("#player-2-rock").removeClass("selected");
+            database.ref().child("/players/player1/ties").set(++player[0].ties);
+            database.ref().child("/players/player2/ties").set(++player[1].ties);
+        } else if (player[1].choice === "player-2-paper") {
+            $("#player-1-rock").removeClass("selected");
+            $("#player-2-paper").removeClass("selected");
+            $("#move-section").text("Paper covers rock.\n" + player[1].name + " wins!");
+            database.ref().child("/players/player1/losses/").set(++player[0].losses);
+            database.ref().child("/players/player2/wins/").set(++player[1].wins);
+        } else if (player[1].choice === "player-2-scissors") {
+            $("#player-1-rock").removeClass("selected");
+            $("#player-2-scissors").removeClass("selected");
+            $("#move-section").text("Rock crushes scissors.\n" + player[0].name + " wins!");
+            database.ref().child("/players/player1/wins/").set(++player[0].wins);
+            database.ref().child("/players/player2/losses/").set(++player[1].losses);
+        }
+    } else if (player[0].choice === "player-1-paper") {
+        if (player[1].choice === "player-2-rock") {
+            $("#player-1-paper").removeClass("selected");
+            $("#player-2-rock").removeClass("selected");
+            $("#move-section").text("Paper covers rock.\n" + player[0].name + " wins!");
+            database.ref().child("/players/player1/wins/").set(++player[0].wins);
+            database.ref().child("/players/player2/losses/").set(++player[1].losses);
+        } else if (player[1].choice === "player-2-paper") {
+            $("#player-1-paper").removeClass("selected");
+            $("#player-2-paper").removeClass("selected");
+            $("#move-section").text("Both play paper.\nIt's a draw!");
+            database.ref().child("/players/player1/ties/").set(++player[0].ties);
+            database.ref().child("/players/player2/ties/").set(++player[1].ties);
+        } else if (player[1].choice === "player-2-scissors") {
+            $("#player-1-paper").removeClass("selected");
+            $("#player-2-scissors").removeClass("selected");
+            $("#move-section").text("Scissors cut paper.\n" + player[1].name + " wins!");
+            database.ref().child("/players/player1/losses/").set(++player[0].losses);
+            database.ref().child("/players/player2/wins/").set(++player[1].wins);
+        }
+    } else if (player[0].choice === "player-1-scissors") {
+        if (player[1].choice === "player-2-rock") {
+            $("#player-1-scissors").removeClass("selected");
+            $("#player-2-rock").removeClass("selected");
+            $("#move-section").text("Rock crushes scissors.\n" + player[1].name + " wins!");
+            database.ref().child("/players/player1/losses").set(++player[0].losses);
+            database.ref().child("/players/player2/wins").set(++player[1].wins);
+        } else if (player[1].choice === "player-2-paper") {
+            $("#player-1-scissors").removeClass("selected");
+            $("#player-2-paper").removeClass("selected");
+            $("#move-section").text("Scissors cut paper.\n" + player[0].name + " wins!");
+            database.ref().child("/players/player1/wins/").set(++player[0].wins);
+            database.ref().child("/players/player2/losses/").set(++player[1].losses);
+        } else if (player[1].choice === "player-2-scissors") {
+            $("#player-1-scissors").removeClass("selected");
+            $("#player-2-scissors").removeClass("selected");
+            $("#move-section").text("Both play scissors.\nIt's a draw!");
+            database.ref().child("/players/player1/ties/").set(++player[0].ties);
+            database.ref().child("/players/player2/ties/").set(++player[1].ties);
+        }
+    }
+    setTimeout(function () {
+        $("#move-section").empty();
+        move = 1;
+        database.ref().child('/move/').set(1);
+    }, 5000);
+}
